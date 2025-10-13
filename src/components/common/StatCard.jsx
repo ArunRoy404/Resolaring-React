ee8bb0b4d05f8dc479eeb9958472af26bcacd777
ArunRoy404 <arunroy0065@gmail.com>
2025-10-13 09:50:04 +0600

    feat: aos installed

--- src/components/common/StatCard.jsx
+++ src/components/common/StatCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import CountUp from 'react-countup';
 
-const StatCard = ({stat, index}) => {
+const StatCard = ({ stat, index }) => {
     return (
-        <div >
+        <div data-aos="fade-up" data-aos-delay={index * 200} >
             <div className={`space-y-2 px-4 flex items-center justify-center ${index % 2 !== 0 ? "border-l border-r" : ''}`}>
                 <div className="max-w-[380px] space-y-4">
                     <h3 className="text-5xl font-bold text-brand-primary">
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from "aos";
 import { Outlet } from 'react-router';
 import NavBar from '../components/shared/NavBar';
 
 const MainLayout = () => {
-    return (
-        <main>
-            <NavBar/>
-            <Outlet />
-        </main>
-    );
-};
+        useEffect(() => {
+            AOS.init({
+                duration: 1000,
+                once: false,
+                offset: 100,
+            });
+        }, []);
 
-export default MainLayout;
+        return (
+            <main>
+                <NavBar />
+                <Outlet />
+            </main>
+        );
+    };
+
+    export default MainLayout;
