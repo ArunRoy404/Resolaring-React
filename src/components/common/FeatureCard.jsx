b8dfb8e4b3e6fa5d4372c5b4135bb32cea351168
ArunRoy404 <arunroy0065@gmail.com>
2025-10-13 12:06:05 +0600

    fix: importing issue fixed

--- src/components/common/FeatureCard.jsx
+++ src/components/common/FeatureCard.jsx
@@ -1,7 +1,7 @@
-import vector from '@/assets/Images/vector/vector.svg'
-import vector1 from '@/assets/Images/vector/vector1.svg'
-import vector2 from '@/assets/Images/vector/vector2.svg'
-import vector3 from '@/assets/Images/vector/vector3.svg'
+import vector from '@/assets/Images/vector/Vector.svg'
+import vector1 from '@/assets/Images/vector/Vector1.svg'
+import vector2 from '@/assets/Images/vector/Vector2.svg'
+import vector3 from '@/assets/Images/vector/Vector3.svg'
 
 const vectors = {
     '#B45C3D0F': vector,
