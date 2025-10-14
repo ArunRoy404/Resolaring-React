26b674af54c59c1c3947677ebc530605e43f9cc4
ArunRoy404 <arunroy0065@gmail.com>
2025-10-14 10:39:06 +0600

    feat: common banner section added for global use

---
+++ src/components/common/CommonSections/CommonBannerSection.jsx
@@ -0,0 +1,63 @@
+import CommonSection from '../CommonSection';
+import { Link } from 'react-router';
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+
+
+const CommonBannerSection = ({ image, title, subtitle, button }) => {
+    return (
+        <CommonSection>
+
+            {/* bg image  */}
+            {
+                !!image &&
+                <img
+                    src={image}
+                    alt="Banner Background"
+                    className="absolute inset-0 -z-[100] w-full h-full object-cover"
+                />
+            }
+
+            {/* Gradient Overlay */}
+            <div className="absolute inset-0 bg-gradient-to-r from-[#071431] to-transparent"></div>
+
+            {/* content  */}
+            <div className='relative z-[100]'>
+                {/* breadcrumb */}
+                <p className='text-white text-lg font-medium'>
+                    {'Home > Why Resolaring?'}
+                </p>
+
+                {/* title  */}
+                <h1 className='text-white font-bold text-6xl mt-4 mb-3'>
+                    {title}
+                </h1>
+
+
+                {/* cta  */}
+                <p className='text-white text-lg mb-8'>
+                    {subtitle}
+                </p>
+
+
+                {/* buttons  */}
+                <div>
+                    {
+                        !!button &&
+                        <Link to={button?.path}>
+                            <Button
+                                size={'lg'}
+                                className={"hover:bg-white/40 py-6 hover:text-white"}
+                            >
+                                {button?.label}
+                                <ArrowRight />
+                            </Button>
+                        </Link>
+                    }
+                </div>
+            </div>
+
+        </CommonSection>
+    );
+};
+export default CommonBannerSection;
--- src/pages/main/AboutPage.jsx
+++ src/pages/main/AboutPage.jsx
@@ -1,14 +1,23 @@
 import BenefitsSection from "@/components/common/CommonSections/BenefitsSection";
+import CommonBannerSection from "@/components/common/CommonSections/CommonBannerSection";
 import CTASection from "@/components/common/CommonSections/CTASection";
 import FeaturesSection from "@/components/common/CommonSections/FeaturesSection";
 import ProcedureSection from "@/components/common/CommonSections/ProcedureSection";
 import TestimonialSection from "@/components/common/CommonSections/TestimonialSection";
-import AboutBannerSection from "@/components/HomeComponents/AboutBannerSection";
+import AboutImage from '@/assets/Images/AboutImage.svg'
 
 const AboutPage = () => {
     return (
         <>
-            <AboutBannerSection />
+            <CommonBannerSection
+                image={AboutImage}
+                title={'Reusing solar energy'}
+                subtitle={"At Resolaring we are committed to giving photovoltaic panels a second life. We make agreements with suppliers and installers so that the panels do not end up in landfills."}
+                button={{
+                    label: 'Browse Marketplace',
+                    path: '/marketplace'
+                }}
+            />
             <FeaturesSection />
             <ProcedureSection />
             <BenefitsSection />
